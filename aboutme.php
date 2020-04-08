9d1e18be0f1f1687bf922d899f8ba05fcdc7f56f
juliusryan25 <63332117+juliusryan25@users.noreply.github.com>
2020-04-08 12:52:12 +0700

    aboutme(menulis nama , kelas , asal sekolah , alamat)

---
+++ aboutme.php
@@ -0,0 +1,6 @@
+<?php
+echo "Nama         : Julius Ryan Listianto <br>";
+echo "Kelas        : X RPL 2  <br>";
+echo "Asal Sekolah : SMK Taruna Bhakti Depok  <br>";
+echo "Alamat       : Jl.Tipar Raya / GG.Haji Benah RT5/RW9 NO.84 Mekarsari - Cimanggis - Depok  <br>";
+?>
